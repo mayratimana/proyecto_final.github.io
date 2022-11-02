8ab7692df45466c3ac4aac26eebcc7c1ee4953a1
Mayra Timaná <117122652+mayratimana@users.noreply.github.com>
2022-11-01 20:59:29 -0500

    initial commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function valida(xusuario,xclave){
+    if(xusuario=="Mayra" && xclave=="1234567"){
+        alert("Usted ha iniciado sesión correctamente.");
+    }else {
+        alert("Incorrecto, vuelva a intentarlo.");
+    }
+}
